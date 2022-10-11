65cbeffb6a3861af62d269f5079764cef8f21c1f
Timur Badretdinov <timurbtr777@gmail.com>
2022-10-11 13:06:32 +0300

    Commit

--- Field_of_magic.jl
+++ Field_of_magic.jl
@@ -2,6 +2,7 @@ Pkg.add("StatsBase")
 using StatsBase
 using Printf
 
+
 struct Field
     items::Array{Any}
 end
@@ -17,6 +18,8 @@ user = Player(chomp(readline()),  parse(Int32, chomp(readline())))
 field = Field([100, 200, 300, 400, "*"])
 println(field.items)
 
+
 function spin_field(Field)
     sample(Field.items)
 end
+
